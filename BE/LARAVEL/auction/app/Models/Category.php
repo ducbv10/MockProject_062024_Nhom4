d47b15814fc65120a877b083cf493566dd0750b8
NguyenNgan03 <simple.270123@gmail.com>
2024-07-12 00:35:40 +0700

    update/feat/crud_NNgan

--- BE/LARAVEL/auction/app/Models/Category.php
+++ BE/LARAVEL/auction/app/Models/Category.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
-    protected $fillable = ['name', 'description'];
+    use HasFactory, SoftDeletes;
+    protected $table = 'Category';
+    //protected $primaryKey = 'CategoryId';
+    protected $fillable = ['CategoryId', 'CategoryName', 'Description', 'DeletedAt'];
+
+    protected $dates = ['DeletedAt'];
+
+    const DELETED_AT = 'DeletedAt';
+    public $timestamps = false;
 }
